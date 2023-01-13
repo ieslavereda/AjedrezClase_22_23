64cfc20b6dc61ec8005485247606add730a5ad7e
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2023-01-13 16:25:17 +0100

    Inicial commit

--- src/main/java/es/ieslavereda/model/Cell.java
+++ src/main/java/es/ieslavereda/model/Cell.java
@@ -16,7 +16,7 @@ public class Cell {
         this.board = board;
         this.piece = null;
         this.original = (
-                (coordinate.getRow()-1+coordinate.getColumn()-'A')%2==0)
+                (coordinate.getRow() - 1 + coordinate.getColumn() - 'A') % 2 == 0)
                 ?
                 Color.WHITE_CELL
                 :
@@ -31,15 +31,17 @@ public class Cell {
     public void setPiece(Piece piece) {
         this.piece = piece;
     }
-    public void highlight(){
-        if(isEmpty()){
-            color = (original==Color.WHITE_CELL)
-                    ?
-                    Color.HIGHLIGHT_WHITE
-                    :
-                    Color.HIGHLIGHT_BLACK;
+
+    public void highlight() {
+        if (isEmpty()) {
+
+            if (original == Color.WHITE_CELL)
+                color = Color.HIGHLIGHT_WHITE;
+            else
+                color = Color.HIGHLIGHT_BLACK;
+
         } else {
-            color = (original==Color.WHITE_CELL)
+            color = (original == Color.WHITE_CELL)
                     ?
                     Color.HIGHLIGHT_KILL_WHITE
                     :
@@ -47,17 +49,18 @@ public class Cell {
         }
     }
 
-    public void resetColor(){
-        color=original;
+    public void resetColor() {
+        color = original;
     }
+
     @Override
-    public String toString(){
-        if(isEmpty())
-            return colorize("   ",color.getAttribute());
+    public String toString() {
+        if (isEmpty())
+            return colorize("   ", color.getAttribute());
         else
-            return colorize(" ",color.getAttribute()) +
+            return colorize(" ", color.getAttribute()) +
                     piece +
-                    colorize(" ",color.getAttribute());
+                    colorize(" ", color.getAttribute());
 
     }
 
@@ -65,17 +68,17 @@ public class Cell {
         return piece == null;
     }
 
-    enum Color{
-        WHITE_CELL(Attribute.BACK_COLOR(180,180,180)),
-        BLACK_CELL(Attribute.BACK_COLOR(100,100,100)),
-        HIGHLIGHT_KILL_WHITE(Attribute.BACK_COLOR(180,0,0)),
-        HIGHLIGHT_KILL_BLACK(Attribute.BACK_COLOR(130,0,0)),
-        HIGHLIGHT_WHITE(Attribute.BACK_COLOR(180,180,0)),
-        HIGHLIGHT_BLACK(Attribute.BACK_COLOR(130,130,0));
+    enum Color {
+        WHITE_CELL(Attribute.BACK_COLOR(180, 180, 180)),
+        BLACK_CELL(Attribute.BACK_COLOR(100, 100, 100)),
+        HIGHLIGHT_KILL_WHITE(Attribute.BACK_COLOR(180, 0, 0)),
+        HIGHLIGHT_KILL_BLACK(Attribute.BACK_COLOR(130, 0, 0)),
+        HIGHLIGHT_WHITE(Attribute.BACK_COLOR(180, 180, 0)),
+        HIGHLIGHT_BLACK(Attribute.BACK_COLOR(130, 130, 0));
 
         private Attribute attribute;
 
-        Color(Attribute attribute){
+        Color(Attribute attribute) {
             this.attribute = attribute;
         }
 
