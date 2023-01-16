29910daee056f19e4f646542789cdb851630992d
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2023-01-16 15:10:35 +0100

    Inicial commit

--- src/main/java/es/ieslavereda/model/Board.java
+++ src/main/java/es/ieslavereda/model/Board.java
@@ -1,4 +1,29 @@
 package es.ieslavereda.model;
 
 public class Board {
+
+    private Cell[][] cells ;
+
+    public Board(){
+        cells = new Cell[8][8];
+
+        for(int row=0;row<=7;row++)
+            for(int col=0;col<=7;col++){
+                cells[row][col]=new Cell(new Coordinate((char)('A'+col),row+1),this);
+            }
+
+    }
+
+    @Override
+    public String toString(){
+        String output ="   A  B  C  D  E  F  G  H\n";
+        for(int row=0;row<=7;row++) {
+            output+=(row+1)+" ";
+            for (int col = 0; col <= 7; col++)
+               output += cells[row][col];
+            output+=" "+(row+1)+"\n";
+        }
+        output +="   A  B  C  D  E  F  G  H";
+        return output;
+    }
 }
