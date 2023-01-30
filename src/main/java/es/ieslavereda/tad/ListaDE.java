4f71eda744159963e2d644c5e9f43b562020f5f4
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2023-01-30 15:50:43 +0100

    Inicial commit

--- src/main/java/es/ieslavereda/tad/ListaDE.java
+++ src/main/java/es/ieslavereda/tad/ListaDE.java
@@ -1,4 +1,96 @@
 package es.ieslavereda.tad;
 
+import es.ieslavereda.model.Piece;
+
 public class ListaDE {
+
+    private int size;
+    private Node head;
+    private Node tail;
+
+    public void addHead(Piece piece){
+        Node node = new Node(piece);
+        // Solo una condicion es necesaria
+        if(size==0 || head==null || tail==null){
+            head = node;
+            tail = node;
+        }else{
+            node.setNext(head);
+            head.setPrevious(node);
+            head=node;
+        }
+        size++;
+    }
+
+    public void addTail(Piece piece){
+
+    }
+
+    public Piece removeHead(){
+        return null;
+    }
+    public Piece removeTail(){
+        return null;
+    }
+    public int count(Piece.PieceType pieceType){
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        String output = "ListaDE { Size: "+ size + ", Values: ";
+        Node aux = head;
+        while (aux!=null){
+            output+= aux + " ";
+            aux = aux.getNext();
+        }
+
+        return output +"}";
+    }
+
+    public String toStringReverse() {
+        String output = "ListaDE { Size: "+ size + ", Values: ";
+        Node aux = tail;
+        while (aux!=null){
+            output+= aux + " ";
+            aux = aux.getPrevious();
+        }
+
+        return output +"}";
+    }
+
+    private class Node {
+        private Piece info;
+        private Node next;
+        private Node previous;
+
+        public Node(Piece info) {
+            this.info = info;
+        }
+
+        public Piece getInfo() {
+            return info;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(info);
+        }
+    }
 }
---
+++ src/test/java/es/ieslavereda/tad/ListaDETest.java
@@ -0,0 +1,36 @@
+package es.ieslavereda.tad;
+
+import es.ieslavereda.model.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListaDETest {
+
+    private static Board board;
+
+    @BeforeAll
+    public static void init(){
+        board = new Board();
+    }
+
+    @Test
+    void addHead() {
+        ListaDE lista = new ListaDE();
+        lista.addHead(new WhiteQueen(new Cell(new Coordinate('a',2),board)));
+        lista.addHead(new BlackQueen(new Cell(new Coordinate('a',2),board)));
+        lista.addHead(new BlackRook(new Cell(new Coordinate('a',2),board)));
+        lista.addHead(new WhiteRook(new Cell(new Coordinate('a',2),board)));
+
+        assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                System.out.println(lista);
+                System.out.println(lista.toStringReverse());
+            }
+        });
+
+    }
+}
